4fdf3e6c607ac4365a4fd84fa6897bc0720f1fa3
Yaadee <ayyeekoo437@gmail.com>
2024-07-08 12:52:43 +0000

    data from fred

---
+++ Code/data_collection/fred_data_collection.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import pandas_datareader.data as web
+from datetime import datetime
+
+# Function to get data from FRED
+def get_fred_data(ticker, start_date, end_date):
+    data = web.DataReader(ticker, 'fred', start_date, end_date)
+    return data
+
+# Define the date range
+start_date = '1988-01-01'
+end_date = datetime.now().strftime('%Y-%m-%d')
+
+# Collect data
+gdp = get_fred_data('GDP', start_date, end_date)
+inflation = get_fred_data('CPIAUCSL', start_date, end_date)
+unemployment = get_fred_data('UNRATE', start_date, end_date)
+exchange_rate = get_fred_data('DEXUSEU', start_date, end_date)
+
+# Save data to CSV
+
+gdp.to_csv('Inputs/data/raw_data/gdp.csv')
+inflation.to_csv('Inputs/data/raw_data/inflation.csv')
+unemployment.to_csv('Inputs/data/raw_data/unemployment.csv')
+exchange_rate.to_csv('Inputs/data/raw_data/exchange_rate.csv')
